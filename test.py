ac183ea7b16461df4c887f3f7aa2ee01d88f01bf
Mikhail Orzhenovsky <mikhail.orzhenovsky@etnatrader.com>
2023-08-08 15:43:04 +0300

    upload test script and github action

---
+++ test.py
@@ -0,0 +1,50 @@
+import sys
+import pandas as pd
+import numpy as np
+
+# Class imbalance ratio
+def CIR(y_before, y_after, verbose=False):
+    expected_ratio = len(y_after) / len(y_before)
+    unique_classes = np.unique(y_before)
+    scores = []
+    weights = []
+
+    for c in unique_classes:
+        count_before = np.sum(y_before == c)
+        count_after = np.sum(y_after == c)
+
+        score = min((count_after / count_before) / expected_ratio, 1)
+        weight = count_before
+
+        scores.append(score)
+        weights.append(weight)
+        
+        if verbose:
+            print(f"Class {c}: score = {score}, weight = {weight}")
+
+    CIR = np.average(scores, weights=weights)
+    return CIR
+
+# Load the data from a Parquet file into a pandas DataFrame.
+data_frame = pd.read_csv(sys.argv[1], index_col=0)
+
+answer = pd.read_csv('submission.csv', header=None)[0]
+
+# Determine the number of top records to consider for computing mean distance.
+top_records_count = int(0.1 * len(data_frame))
+
+if len(answer) != top_records_count:
+    print(f'Submission file must include {top_records_count} ids. Found {len(answer)} ids.')
+    raise ValueError()
+    
+sorted_data_frame = data_frame.loc[answer]
+
+
+# Compute and print the mean of the 'distance' column for the top 10% records.
+mean_distance = sorted_data_frame.iloc[:top_records_count]['distance'].mean()
+#print(mean_distance)
+
+# Compute and print Class Imbalance Ratio
+cir = CIR(data_frame['label'], sorted_data_frame.iloc[:top_records_count]['label'])
+
+print(f'Submission scores: Mean distance={mean_distance:.2f}, Class Imbalance Ratio={cir:.2f}')
