0a5642401d24b2391b5070c60c58d990f40cf425
yauheni.dzirvuk <yauheni.dzirvuk@wizart.ai>
2023-11-23 19:56:09 +0300

    submission

--- baseline.py
+++ baseline.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import numpy as np
+import sys
+import catboost as cb
+import pickle
+
 
 def softmax(x):
     # Compute the exponential values for each element in the input array
@@ -12,23 +16,44 @@ def softmax(x):
 # Load the data from a Parquet file into a pandas DataFrame.
 data_frame = pd.read_parquet(sys.argv[1])
 
+# Load model and extra files
+model = cb.CatBoostRegressor()
+model.load_model('models/reg.pkl')
+with open('models/class_acc.pkl', 'rb') as fp:
+    class_acc = pickle.load(fp)
+with open('models/class_mean_dist.pkl', 'rb') as fp:
+    class_mean_dist = pickle.load(fp)
+
+TOP_CLASSES_COUNT = 6
 # Initialize an empty list to store the maximum confidence values.
-max_confidences = []
+softmax_values_all = []
 
 # Iterate over the DataFrame rows.
 for _, row in data_frame.iterrows():
     # Compute softmax for the 'raw_prediction' column of the current row.
-    softmax_values = softmax(row['raw_prediction'])
-    
-    # Find the maximum confidence value and append it to the list.
-    max_confidences.append(softmax_values.max())
+    sofmax_row = softmax(row['raw_prediction'])
+
+    softmax_values = np.sort(sofmax_row)[-TOP_CLASSES_COUNT:]
+    softmax_values_all.append(softmax_values)
+
+data_frame['pred'] = [x.argmax() for x in data_frame['raw_prediction']]
+data_frame['acc_label'] = data_frame['pred'].apply(lambda x: class_acc[x])
+data_frame['mean_label_dist'] = data_frame['pred'].apply(lambda x: class_mean_dist[x])
 
 # Add a new column 'confidence' to the DataFrame using the list of maximum confidence values.
-data_frame['confidence'] = max_confidences
+data_frame['softmax'] = softmax_values_all
+columns = ['cl_{}'.format(x + 1) for x in range(TOP_CLASSES_COUNT)]
+data_frame[columns] = data_frame['softmax'].copy().tolist()
+
 data_frame['pred'] = [x.argmax() for x in data_frame['raw_prediction']]
 
+X = data_frame.drop(['text', 'coordinates', 'label', 'raw_prediction', 'softmax', 'distance', 'confidence'],
+                    axis=1)
+pred = model.predict(X)
+data_frame['score'] = pred
+
 # Sort the DataFrame by 'confidence' in descending order.
-sorted_data_frame = data_frame.sort_values(by='confidence', ascending=False)
+sorted_data_frame = data_frame.sort_values(by='score', ascending=True)
 
 # Determine the number of top records to consider for computing mean distance.
 top_records_count = int(0.1 * len(data_frame))
